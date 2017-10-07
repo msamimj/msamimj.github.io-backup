a90bf12f36e6f130f87f7cb2f24b797848a1a66b
Md Samim Jahin <msamimj@gmail.com>
2017-10-07 16:06:02 +0530

    fixed incand overflow

--- 18/js/main.js
+++ 18/js/main.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-        var vwidth = window.innerWidth,
-        vheight = window.innerHeight,
+    var vwidth = window.innerWidth,
+    vheight = window.innerHeight,
         cwidth = 0, //width of container containing the player components
         cheight = 0; //height of container containing the player components
 
@@ -18,40 +18,40 @@ window.onload = function(){
     }
 
     var ratio = cwidth/600,
-        ratio2 = cwidth/2000;
+    ratio2 = cwidth/2000;
 
     var container = document.getElementById("container");
 
     var     c0  =    document.getElementById("c0"),
-            bts =    document.getElementsByClassName("button-top"),
-            c1  =    document.getElementById("c1"),
-            c2  =    document.getElementById("c2"),
-            c3  =    document.getElementById("c3"),
-            c4  =    document.getElementById("c4"),
-            c5  =    document.getElementById("c5"),
-            c6  =    document.getElementById("c6"),
-            c7  =    document.getElementById("c7"),
-            c8  =    document.getElementById("c8"),
-            c9  =    document.getElementById("c9"),
-            c10 =    document.getElementById("c10"),
-            c11 =    document.getElementById("c11"),
-            c12 =    document.getElementById("c12"),
-            c13 =    document.getElementById("c13"),
-            c14 =    document.getElementById("c14"),
-            c15 =    document.getElementById("c15"),
-            c16 =    document.getElementById("c16"),
-            c17 =    document.getElementById("c17"),
-            c18 =    document.getElementById("c18"),
-            c19 =    document.getElementById("c19"),
-            c20 =    document.getElementById("c20"),
-            c21 =    document.getElementById("c21"),
-            c22 =    document.getElementById("c22"),
-            c23 =    document.getElementById("c23"),
-            c24 =    document.getElementById("c24"),
-            wel =    document.getElementById("welcome"),
-            com =    document.getElementById("coming"),
-            inc =    document.getElementById("incand"),
-            down =   document.getElementById("btn-down");
+    bts =    document.getElementsByClassName("button-top"),
+    c1  =    document.getElementById("c1"),
+    c2  =    document.getElementById("c2"),
+    c3  =    document.getElementById("c3"),
+    c4  =    document.getElementById("c4"),
+    c5  =    document.getElementById("c5"),
+    c6  =    document.getElementById("c6"),
+    c7  =    document.getElementById("c7"),
+    c8  =    document.getElementById("c8"),
+    c9  =    document.getElementById("c9"),
+    c10 =    document.getElementById("c10"),
+    c11 =    document.getElementById("c11"),
+    c12 =    document.getElementById("c12"),
+    c13 =    document.getElementById("c13"),
+    c14 =    document.getElementById("c14"),
+    c15 =    document.getElementById("c15"),
+    c16 =    document.getElementById("c16"),
+    c17 =    document.getElementById("c17"),
+    c18 =    document.getElementById("c18"),
+    c19 =    document.getElementById("c19"),
+    c20 =    document.getElementById("c20"),
+    c21 =    document.getElementById("c21"),
+    c22 =    document.getElementById("c22"),
+    c23 =    document.getElementById("c23"),
+    c24 =    document.getElementById("c24"),
+    wel =    document.getElementById("welcome"),
+    com =    document.getElementById("coming"),
+    inc =    document.getElementById("incand"),
+    down =   document.getElementById("btn-down");
 
     var tl = new TimelineMax({delay:0.1, onComplete: unhidePage2});
 
@@ -144,6 +144,7 @@ window.onload = function(){
     };
 
     down.onclick = function(){
+        // console.log("down");
         scroll = window.pageYOffset;
         scrollToPage2();
     };
