7a53a6ff0493fd1a8a5d1b654b55374befd1f649
msamimj <msamimj@gmail.com>
2018-03-19 07:40:50 +0530

    ssl v2

--- ssl/scripts/main.js
+++ ssl/scripts/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('scroll', function(){
 	if(window.pageYOffset>(document.getElementById('div0').offsetHeight-(0.05*window.innerHeight))){
 		document.getElementById('logo').className = 'stick';
-	}else if(window.pageYOffset > 100){
+	}else if(window.pageYOffset > window.innerHeight*0.65){
 		document.getElementById('logo').className = 'appear';
 	}else{
 		document.getElementById('logo').className = '';
