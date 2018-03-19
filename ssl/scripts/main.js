5b8edc67aabe49134aa3709992a58e56d0dcc21c
msamimj <msamimj@gmail.com>
2018-03-19 09:15:10 +0530

    ssl v2.1

--- ssl/scripts/main.js
+++ ssl/scripts/main.js
@@ -7,3 +7,36 @@ document.addEventListener('scroll', function(){
 		document.getElementById('logo').className = '';
 	}
 })
+
+var div4 = document.getElementById('div4');
+
+var testimonials = document.getElementsByClassName('testimonial');
+
+var testimonialsDiv = document.getElementById('testimonials');
+
+// testimonialsDiv.style.width = testimonials.length * 0.7 * window.innerWidth + 'px';
+testimonialsDiv.style.height = testimonials[0].scrollHeight + .08*window.innerWidth + 'px';
+
+for(var i=0; i<testimonials.length; i++){
+	testimonials[i].style.left = (i * 0.7 * window.innerWidth) + 'px';
+}
+
+var right = document.getElementById('right-test');
+
+right.addEventListener('click', nextTest);
+
+function nextTest(){
+	var curr = testimonialsDiv.scrollLeft;
+	var next = curr + 0.7*innerWidth;
+	testimonialsDiv.scrollTo(next, 0);
+}
+
+var left = document.getElementById('left-test');
+
+left.addEventListener('click', prevTest);
+
+function prevTest(){
+	var curr = testimonialsDiv.scrollLeft;
+	var prev = curr - 0.7*innerWidth;
+	testimonialsDiv.scrollTo(prev, 0);
+}
